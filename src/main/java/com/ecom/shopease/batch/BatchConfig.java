a6b392cf0c7229766cfd204634a69a51f4a5bee8
Said EL ARISS EL IDRISSI <saidariss3@gmail.com>
2025-01-09 23:37:36 +0100

    add spring batch 5 config

--- src/main/java/com/ecom/shopease/batch/BatchConfig.java
+++ src/main/java/com/ecom/shopease/batch/BatchConfig.java
@@ -1,28 +1,33 @@
-﻿package com.ecom.shopease.batch;
+package com.ecom.shopease.batch;
 
+import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemReader;
-import org.springframework.batch.item.ItemWriter;
-import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
-@Configurable
-@EnableBatchProcessing
+@Configuration
 public class BatchConfig {
-//    private TransactionManager transactionManager;
-//
-//    @Bean
-//    public Step step(StepBuilder stepBuilder, ItemReader<String> reader,
-//                     ItemProcessor<String,String> processor,
-//                     ItemWriter<String> writer){
-//        return stepBuilder.<String,String>chunk(10)
-//                .reader(reader)
-//                .processor(processor)
-//                .writer(writer)
-//                .build();
-//
-//    }
+
+    @Bean
+    public Job job(JobRepository jobRepository, Step step) {
+        return new JobBuilder("job", jobRepository)
+                .start(step)
+                .build();
+    }
+
+    @Bean
+    public Step step(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("step", jobRepository)
+                .tasklet((StepContribution contribution, ChunkContext chunkContext) -> {
+                    System.out.println("hello, friends!");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager).build();
+    }
 }
